284d2babec8958f2027461a49c0245b6fed665d6
Rajan Joshi <rajan.joshi@onthebeach.co.uk>
2020-02-14 23:15:11 +0000

    Add landing page and images.

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200213135413) do
+ActiveRecord::Schema.define(version: 20200213203503) do
 
-  create_table "customers", force: :cascade do |t|
-    t.string "customer_name"
-    t.string "email"
-    t.string "phone_number"
+  create_table "customers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "c_address"
-    t.boolean "admin?", default: false
+    t.string "phone_number"
+    t.boolean "is_admin", default: false, null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table "deliveries", force: :cascade do |t|
+  create_table "deliveries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "delivery_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "menus", force: :cascade do |t|
+  create_table "menus", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "food_name"
     t.text "food_info"
     t.decimal "price", precision: 8, scale: 2
@@ -37,7 +43,7 @@ ActiveRecord::Schema.define(version: 20200213135413) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "orders", force: :cascade do |t|
+  create_table "orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.decimal "price_total", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
